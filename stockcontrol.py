4e445a846f63cf48b344ae0c7ebab7bba904d770
EDWINBEKA <42927308+EDWINBEKA@users.noreply.github.com>
2018-10-14 22:30:26 +0300

    Update stockcontrol.py

--- stockcontrol.py
+++ stockcontrol.py
@@ -8,6 +8,7 @@
 
 ## NAME: KATEETE  TWAHA    NO.: 1800737883    REG.NO: 2018/HD05/1958U    
 ## NAME: WAKIBI CHRISTOPHER NO.: 1800740236     REG. NO: 2018/HD05/1983U
+## NAME: BEINOMUGISHA EDWIN NO: 1800737351   REG.NO: 2018/HD05/1948U
 
 from datetime import date
 
@@ -177,7 +178,7 @@ for barcode in ['1234','2312','1112','1111','2312','1191','0191','2312']:
     try:
         stockctrl.sellStock(barcode)    
     except SoldOutOfStockError as (e):
-        print("Stock sold which isn't in stock:" + e.item.toString())
+        print("Stock sold which isn't in stock:" + str(e.item))
     except ItemNotFoundError as (e):
         print("Item not found:" + e.barcode)
 
